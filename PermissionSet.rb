b73dd8d17e9012c4a8dcfbf804e3ae5a53eeb9ae
SkyBirdSoar <skypandafusion@yahoo.com.sg>
2014-01-01 22:32:59 +0800

    Added inspect and to_str

--- PermissionSet.rb
+++ PermissionSet.rb
@@ -4,8 +4,9 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'Permissible')
 class PermissionSet
   include Permissible
 
-  def initialize(permissions)
+  def initialize(name, permissions)
     super(permissions)
+    @name = name
     @listeners = []
   end
 
@@ -15,5 +16,13 @@ class PermissionSet
 
   def update(event, info)
     @listeners.each { |x| x.notify(event, info) }
+  end
+
+  def inspect
+    "#<PermissionSet:0x#{'%x' % (self.object_id << 1)} Owner=#{@name} Permission_Count=#{@permissions.length}>"
+  end
+
+  def to_str
+    @name
   end
 end
