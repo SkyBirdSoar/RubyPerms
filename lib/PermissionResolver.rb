bb4a6148cca62cb54dca1d586d31959ea9b3a063
SkyBirdSoar <skypandafusion@yahoo.com.sg>
2014-01-07 22:37:15 +0800

    Oh, there will be a PermissionResolver too.

---
+++ lib/PermissionResolver.rb
@@ -0,0 +1,8 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), 'WildPermission')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'WildHelper')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'Permission')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'PermissionHelper')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'PermissionCluster')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'ClusterHelper')
+module PermissionResolver
+end
